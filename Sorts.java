a2936e97235ca29494c14dca4ac605ba96fb81d0
Matthew Price <doggy1199@yahoo.com>
2013-02-28 07:47:41 -0700

    Created randomArray method

---
+++ Sorts.java
@@ -0,0 +1,23 @@
+/**
+ * Sorts
+ *
+ * 		Quadratic Sorts
+ */
+class Sorts {
+
+	/**
+	 * random Array
+	 *
+	 * 		returns a random Array with given length
+	 *
+	 * @param  length - length of the returned array
+	 * @param  min - the min for the randomly selected numbers
+	 * @param  max - the max for the randomly selected numbers
+	 */
+	public int[] randomArray(int length, int min, int max) {
+		int[] answer = new int[length];
+		
+	}
+}
+
+
