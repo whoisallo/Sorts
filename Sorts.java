f85a48dc6273954f7baf6d4f21705334ac67edf9
Matthew Price <doggy1199@yahoo.com>
2013-03-05 08:24:07 -0700

    Created insertion and selection sort

--- Sorts.java
+++ Sorts.java
@@ -41,13 +41,52 @@ class Sorts {
 	}
 
 	/**
+	 * bubbleSort
+	 */
+	public static int[] bubbleSort(int[] rand) {
+		
+	}
+
+	/**
+	 * insertionSort
+	 */
+	public static int[] insertionSort(int[] rand) {
+		for(int i = rand.length-1; i >0; i++) {
+			
+		}
+	}
+
+	/**
+	 * selectionSort
+	 *
+	 * 		sorts an Array
+	 */
+	public static int[] selectionSort(int[] rand) {
+		int low = rand[0];
+		int counter = 0;
+		for(int i = 0; i < rand.length; i++) {
+			low = rand[i];
+			for(int j = i; j<rand.length; j++) {
+				if(rand[j] < low) {
+					low = rand[j];
+					counter = j;
+				}
+			}
+			rand[counter] = rand[i];
+			rand[i] = low;
+		}
+		return rand;
+	}
+
+	/**
 	 * main
 	 *
 	 * 		main method
 	 */
 	
 	public static void main(String[] args) {
-		int[] thingyToPrint = randomArray(10, 2, 6);
+		int[] thingyToPrint = randomArray(100000, 0, 1);
+		//thingyToPrint = sortArray(thingy);
 		print(thingyToPrint);
 		System.out.println();
 	}
